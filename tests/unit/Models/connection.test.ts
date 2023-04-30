880f9c9c19be42702c28758c781788654355aabb
Bete.mrt <bete.mrt@gmail.com>
2023-04-30 00:56:40 -0300

    Package.json copy

--- tests/unit/Models/connection.test.ts
+++ tests/unit/Models/connection.test.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import sinon from 'sinon';
-import { describe, afterEach } from 'mocha';
+import { describe, afterEach, beforeEach } from 'mocha';
 import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import connectToDatabase from '../../../src/Models/Connection';
@@ -8,24 +8,30 @@ import connectToDatabase from '../../../src/Models/Connection';
 chai.use(chaiAsPromised);
 
 describe('connectToDatabase', function () {
+  let connectStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    connectStub = sinon.stub(mongoose, 'connect');
+  });
+
   afterEach(() => {
-    sinon.restore();
+    connectStub.restore();
   });
 
   it('should connect to the database successfully', async function () {
-    sinon.stub(mongoose, 'connect').resolves();
+    connectStub.resolves();
 
     await expect(connectToDatabase()).to.be.fulfilled;
 
-    sinon.assert.calledOnce(sinon.stub(mongoose, 'connect'));
+    sinon.assert.calledOnce(connectStub);
   });
 
   it('should throw an error if the connection fails', async function () {
     const errorMessage = 'Connection error';
-    sinon.stub(mongoose, 'connect').rejects(new Error(errorMessage));
+    connectStub.rejects(new Error(errorMessage));
 
     await expect(connectToDatabase()).to.be.rejectedWith(errorMessage);
 
-    sinon.assert.calledOnce(sinon.stub(mongoose, 'connect'));
+    sinon.assert.calledOnce(connectStub);
   });
 });
